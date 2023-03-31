7f1b28c1524875fa04e83330c646e4edaaa86f0a
ThomasDebek <tom.debek@gmail.com>
2023-03-31 15:30:17 +0200

    task_2: done

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,25 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def check_password
+    return false unless @password.length.between?(6, 24)
+    return false unless @password.match?(/[A-Z]/)
+    return false unless @password.match?(/[a-z]/)
+    return false unless @password.match?(/\d/)
+    return false unless @password.match?(/[!@#\$%&\*\+=:;?<>\(\)]/)
+    return false if @password.scan(/(.)\1+/).flatten.any? { |s| s.length > 2 }
+    true
+  end
 end
+
+password_one = "Password1!"
+password_second = "password1!"
+
+p first = ValidatePassword.new(password_one).check_password
+
+p second = ValidatePassword.new(password_second).check_password
+
+
+
